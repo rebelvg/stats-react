d8e569e702d5c2923e464208c3d53eb4a01826af
rebelvg <johnhartigan@ya.ru>
2017-11-21 17:26:35 +0200

    Graph changes.

--- src/modules/Stream/Components/StreamWrapper.js
+++ src/modules/Stream/Components/StreamWrapper.js
@@ -10,6 +10,8 @@ import streamsTable from '../../Shared/Tables/StreamsTable';
 import subscribersTable from '../../Shared/Tables/SubscribersTable';
 import defaultFilterMethod from '../../Shared/Methods/TableFilter';
 
+const tickFormatter = (tick) => moment.unix(tick).format('ddd HH:mm');
+
 class StreamWrapper extends Component {
     render() {
         const {stream = null, subscribers = [], events = []} = this.props;
@@ -31,15 +33,16 @@ class StreamWrapper extends Component {
             <ResponsiveContainer width='100%' height={450}>
                 <LineChart
                     data={_.map(events, (event) => {
-                        event.time = moment(event.time).format('ddd HH:mm');
+                        event.unixTime = moment(event.time).unix();
                         event.subscribersCount = event.subscribers.length;
                         return event;
                     })}
                     margin={{top: 20, right: 50, left: 5, bottom: 5}}
                 >
-                    <XAxis dataKey="time"/>
+                    <XAxis dataKey="unixTime" tickFormatter={tickFormatter} type="number" scale="time"
+                           domain={['dataMin', 'dataMax']}/>
                     <YAxis/>
-                    <Tooltip/>
+                    <Tooltip labelFormatter={(value) => moment.unix(value).format('ddd HH:mm')}/>
                     <Legend/>
                     <Line name="Viewers" type="monotone" dataKey="subscribersCount" stroke="#82ca9d"/>
                 </LineChart>
